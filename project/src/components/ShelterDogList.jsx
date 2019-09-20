d2f8c21601aa28b0232b33a4c01fa698484c6675
Lisa Jensen <leese_38@yahoo.com>
2019-09-20 11:16:19 -0700

    add ShelterDog and ShelterDogList base component files

---
+++ project/src/components/ShelterDogList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShelterDog from './ShelterDog';
+import PropTypes from 'prop-types';
+
+function ShelterDogList(props) {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default ShelterDogList;
