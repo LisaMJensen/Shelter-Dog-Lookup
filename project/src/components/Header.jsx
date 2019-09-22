d8320bf85cab4819aada06e70d8da0bc40a7d7a3
Lisa Jensen <leese_38@yahoo.com>
2019-09-22 13:14:58 -0700

    add reactstrap navbar

--- project/src/components/Header.jsx
+++ project/src/components/Header.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import '../assets/styles/Header.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import placeholderLogo from '../assets/images/placeholderLogo.png';
+import {
+    Navbar,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from 'reactstrap';
 
 function Header() {
     return (
         <div>
-            <h1><img src={placeholderLogo} alt="placeholder dog logo" />
-                Placeholder Name</h1>
+                <Navbar color="light" light expand="md">
+                <NavbarBrand href="/"><h3><img src={placeholderLogo} alt="placeholder dog logo" />
+                    Placeholder Name</h3></NavbarBrand>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                    </Nav>
+                </Navbar>
+            
         </div>
     );
 }
