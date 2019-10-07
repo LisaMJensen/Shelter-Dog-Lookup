ba56192d9d0665c6891b3f9adeb0df123199fce7
Lisa Jensen <leese_38@yahoo.com>
2019-10-07 15:48:18 -0700

    add masterShelterDogList

--- project/src/components/ShelterDogList.jsx
+++ project/src/components/ShelterDogList.jsx
@@ -2,12 +2,35 @@ import React from 'react';
 import ShelterDog from './ShelterDog';
 import PropTypes from 'prop-types';
 
+var masterShelterDogList = [
+    {
+        breed: '',
+        age: '',
+        location: '',
+    },
+    {
+        breed: '',
+        age: '',
+        location: '',
+    }, 
+    {
+        breed: '',
+        age: '',
+        location: '',
+    },
+];
+
 function ShelterDogList(props) {
     return (
-        <ShelterDog
-        breed=""
-        age=""
-        location=""/>
+        <div>
+        {masterShelterDogList.map((dog, index) =>
+            <ShelterDog
+                breed={dog.breed}
+                age={dog.age}
+                location={dog.location}
+                key={index} />
+            )}
+        </div>
     );
 }
 
