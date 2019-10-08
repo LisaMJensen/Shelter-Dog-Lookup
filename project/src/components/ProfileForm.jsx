a144806d50f50050030ce3b343754f9816de2461
Lisa Jensen <leese_38@yahoo.com>
2019-10-08 10:14:50 -0700

    add initial rework of form on new branch

--- project/src/components/ProfileForm.jsx
+++ project/src/components/ProfileForm.jsx
@@ -2,57 +2,80 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
-require('dotenv').config();
 
-class ProfileForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            location: '',
-        };
+// require('dotenv').config();
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-    }
-
-    handleChange(event) {
-        this.setState({ location: event.target.value });
-    }
+function ProfileForm(props) {
+    let _location = null;
 
 
-    handleSubmit(event, dogs) {
-
-        const API_KEY = process.env.REACT_APP_API_KEY;
-        const SECRET = process.env.REACT_APP_SECRET;
-        var petfinder = require("@petfinder/petfinder-js");
-        var client = new petfinder.Client({ apiKey: API_KEY, secret: SECRET });
-        var dogs = [];
-
-        client.animal.search({ type: "Dog", breed: "poodle", location: this.state.location, status: "adoptable", limit: "10" })
-            .then(function (response) {
-                for (let i = 0; i <= response.data.animals.length; i++) {
-                    console.log(response.data.animals[i].name);
-
-                    dogs.push(response.data.animals[i]);
-                    
-                    console.log(dogs);
-                    
-                    // console.log(response.data.animals[i].breeds);
-                    // console.log(response.data.animals[i].age);
-                    // console.log(response.data.animals[i].photos[0]);
-                }
-                
-            })
-            .catch(function (error) {
-                console.log("There was an error");
-            });
-
+    function handleProfileFormSubmission(event) {
+        const { dispatch } = props;
         event.preventDefault();
+        const action = {
+            location: _location.value,
+           
+        };
+        dispatch(action);
+        _location.value = '';
+        
+}
 
-    }
 
-    render() {
+
+
+
+
+
+// {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             location: '',
+//         };
+
+//         this.handleChange = this.handleChange.bind(this);
+//         this.handleSubmit = this.handleSubmit.bind(this);
+
+//     }
+
+//     handleChange(event) {
+//         this.setState({ location: event.target.value });
+//     }
+
+
+//     handleSubmit(event, dogs) {
+
+//         const API_KEY = process.env.REACT_APP_API_KEY;
+//         const SECRET = process.env.REACT_APP_SECRET;
+//         var petfinder = require("@petfinder/petfinder-js");
+//         var client = new petfinder.Client({ apiKey: API_KEY, secret: SECRET });
+//         var dogs = [];
+
+//         client.animal.search({ type: "Dog", breed: "poodle", location: this.state.location, status: "adoptable", limit: "10" })
+//             .then(function (response) {
+//                 for (let i = 0; i <= response.data.animals.length; i++) {
+//                     console.log(response.data.animals[i].name);
+
+//                     dogs.push(response.data.animals[i]);
+                    
+//                     console.log(dogs);
+                    
+//                     // console.log(response.data.animals[i].breeds);
+//                     // console.log(response.data.animals[i].age);
+//                     // console.log(response.data.animals[i].photos[0]);
+//                 }
+                
+//             })
+//             .catch(function (error) {
+//                 console.log("There was an error");
+//             });
+
+//         event.preventDefault();
+
+//     }
+
+    
         return (
             <div>
                 <h2>Lifestyle/Personality Profile</h2>
@@ -76,11 +99,11 @@ class ProfileForm extends React.Component {
             </div>
         );
 
-    }
-}
+        
+
 
 ProfileForm.propTypes = {
     onNewProfileTagsCreation: PropTypes.func
 };
 
-export default ProfileForm;
+export default connect()(ProfileForm);
